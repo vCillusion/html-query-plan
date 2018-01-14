f95e816b10adec710a03441cadc7b18c2ad3c90b
Justin Pealing <justin@pealing.me.uk>
2018-01-14 22:11:35 +0000

    Functions to get plan XML

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 ﻿import * as transform from './transform';
 import { drawSvgLines } from './svgLines';
 import { initTooltip } from './tooltip';
+import { hasClass, findAncestor } from './utils';
 
 declare function require(path: string) : any;
 let qpXslt = require('raw-loader!./qp.xslt');
@@ -17,9 +18,54 @@ function showPlan(container: Element, planXml: string, options?: Options) {
     transform.setContentsUsingXslt(container, planXml, qpXslt);
     drawSvgLines(container);
 
+    let xml = new DOMParser().parseFromString(planXml, "text/xml");
+    container["qp-xml"] = xml;
+
     if (options.jsTooltips) {
         initTooltip(container);
     }
+}
+
+function getPlanXml(container: Element): XMLDocument {
+    return container["qp-xml"];
+}
+
+function findContainer(node: Element) {
+    let root = findAncestor(node, "qp-root");
+    return root ? root.parentElement : null;
+}
+
+function findStatementId(node: HTMLElement) {
+    while ((node = node.parentElement) && node && !node.dataset["statementId"]);
+    return node ? node.dataset["statementId"] : null;
+}
+
+function getNodeXml(node: HTMLElement) {
+    if (!hasClass(node, "qp-node")) {
+        return null;
+    }
+    let container = findContainer(node);
+    if (container == null) {
+        return null;
+    }
+    let statementId = findStatementId(node);
+    let nodeId = node.dataset["nodeId"];
+    let planXml = getPlanXml(container);
+    let nodeXml = planXml.evaluate(
+        `//showplan:*[@StatementId='${statementId}']//showplan:RelOp[@NodeId='${nodeId}']`,
+        planXml, <any>nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
+    return {
+        nodeId,
+        statementId,
+        nodeXml
+    }
+}
+
+function nsResolver(prefix) {
+    var ns = {
+      'showplan' : 'http://schemas.microsoft.com/sqlserver/2004/07/showplan'
+    };
+    return ns[prefix] || null;
 }
 
 function setDefaults(options: Options, defaults: Options) {
@@ -37,4 +83,10 @@ function setDefaults(options: Options, defaults: Options) {
     return ret;
 }
 
-export { drawSvgLines as drawLines, showPlan }
+export {
+    drawSvgLines as drawLines,
+    showPlan,
+    getPlanXml,
+    getNodeXml,
+    nsResolver
+}
